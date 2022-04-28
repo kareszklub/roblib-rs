e8bc9406da9cbecf239d23f7c93b302054052153
beni69 <73666440+beni69@users.noreply.github.com>
2022-04-28 13:48:28 +0200

    roland: move robot code to new file

---
+++ roland/src/gpio.rs
@@ -0,0 +1,42 @@
+pub use rppal::gpio::{Error, Gpio};
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub fn try_init(gpio: &Gpio) -> Result<()> {
+    // will attempt to initialize all the pins just to see if they work
+    gpio.get(LED_R)?;
+    gpio.get(LED_G)?;
+    gpio.get(LED_B)?;
+
+    Ok(())
+}
+
+pub fn led(gpio: &Gpio, r: bool, g: bool, b: bool) -> Result<()> {
+    let mut pin_r = gpio.get(LED_R)?.into_output();
+    let mut pin_g = gpio.get(LED_G)?.into_output();
+    let mut pin_b = gpio.get(LED_B)?.into_output();
+
+    // don't reset the pins when these variables go out of scope
+    pin_r.set_reset_on_drop(false);
+    pin_g.set_reset_on_drop(false);
+    pin_b.set_reset_on_drop(false);
+
+    if r {
+        pin_r.set_high();
+    } else {
+        pin_r.set_low();
+    }
+    if g {
+        pin_g.set_high();
+    } else {
+        pin_g.set_low();
+    }
+    if b {
+        pin_b.set_high();
+    } else {
+        pin_b.set_low();
+    }
+
+    Ok(())
+}
+
+// TODO
