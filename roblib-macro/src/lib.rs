5d9fc82598fa394c9a0606bf96c8cc6cf4938be7
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2023-06-30 20:26:49 +0200

    Update macros

--- roblib-macro/src/lib.rs
+++ roblib-macro/src/lib.rs
@@ -15,7 +15,7 @@ fn get_readable_enum(ident: &Ident, data: &syn::DataEnum) -> TokenStream {
         match data.variants.len() {
             0 => abort!(
                 ident.span(),
-                "Can't implement Writable for enum without variants"
+                "Can't implement ::roblib_parsing::Writable for enum without variants"
             ),
             0x01..=0xFF => "u8",
             0x01_00..=0xFF_FF => "u16",
@@ -56,13 +56,13 @@ fn get_readable_enum(ident: &Ident, data: &syn::DataEnum) -> TokenStream {
             let ty = &field.ty;
 
             variant_text.append_all(quote! {
-                let #name: #ty = Readable::parse_text(s)?;
+                let #name: #ty = ::roblib_parsing::Readable::parse_text(s)?;
             });
             variant_binary.append_all(quote! {
-                let #name: #ty = Readable::parse_binary(r)?;
+                let #name: #ty = ::roblib_parsing::Readable::parse_binary(r)?;
             });
             variant_binary_async.append_all(quote! {
-                let #name: #ty = Readable::parse_binary_async(r).await?;
+                let #name: #ty = ::roblib_parsing::Readable::parse_binary_async(r).await?;
             });
         }
 
@@ -97,16 +97,16 @@ fn get_readable_enum(ident: &Ident, data: &syn::DataEnum) -> TokenStream {
 
     let res = quote! {
         #[cfg_attr(feature = "async", ::async_trait::async_trait)]
-        impl Readable for #ident {
+        impl ::roblib_parsing::Readable for #ident {
             fn parse_text<'a>(s: &mut impl ::std::iter::Iterator<Item = &'a str>) -> ::anyhow::Result<Self> {
-                let tag: #tag_type = Readable::parse_text(s)?;
+                let tag: #tag_type = ::roblib_parsing::Readable::parse_text(s)?;
                 match tag {
                     #text
                     _ => ::std::result::Result::Err(::anyhow::Error::msg("Unknown enum variant"))
                 }
             }
             fn parse_binary(r: &mut impl ::std::io::Read) -> ::anyhow::Result<Self> {
-                let tag: #tag_type = Readable::parse_binary(r)?;
+                let tag: #tag_type = ::roblib_parsing::Readable::parse_binary(r)?;
                 match tag {
                     #binary
                     _ => ::std::result::Result::Err(::anyhow::Error::msg("Unknown enum variant"))
@@ -114,7 +114,7 @@ fn get_readable_enum(ident: &Ident, data: &syn::DataEnum) -> TokenStream {
             }
             #[cfg(feature = "async")]
             async fn parse_binary_async(r: &mut (impl ::futures::AsyncRead + Send + Unpin)) -> ::anyhow::Result<Self> {
-                let tag: #tag_type = Readable::parse_binary_async(r).await?;
+                let tag: #tag_type = ::roblib_parsing::Readable::parse_binary_async(r).await?;
                 match tag {
                     #binary_async
                     _ => ::std::result::Result::Err(::anyhow::Error::msg("Unknown enum variant"))
@@ -145,13 +145,13 @@ fn get_readable_struct(struct_ident: &Ident, struct_data: &syn::DataStruct) -> T
 
         field_names.append_all(quote! { #name, });
         text.append_all(quote! {
-            let #name = Readable::parse_text(s)?;
+            let #name = ::roblib_parsing::Readable::parse_text(s)?;
         });
         binary.append_all(quote! {
-            let #name = Readable::parse_binary(r)?;
+            let #name = ::roblib_parsing::Readable::parse_binary(r)?;
         });
         binary_async.append_all(quote! {
-            let #name = Readable::parse_binary_async(r).await?;
+            let #name = ::roblib_parsing::Readable::parse_binary_async(r).await?;
         });
     }
 
@@ -163,7 +163,7 @@ fn get_readable_struct(struct_ident: &Ident, struct_data: &syn::DataStruct) -> T
 
     let res = quote! {
         #[cfg_attr(feature = "async", ::async_trait::async_trait)]
-        impl Readable for #struct_ident {
+        impl ::roblib_parsing::Readable for #struct_ident {
             fn parse_text<'a>(s: &mut impl ::std::iter::Iterator<Item = &'a str>) -> ::anyhow::Result<Self> {
                 #text
                 Ok(#ret)
@@ -231,20 +231,20 @@ fn get_writable_struct(struct_ident: &Ident, struct_data: &syn::DataStruct) -> T
         };
 
         text.append_all(quote! {
-            #name.write_text(f)?;
+            ::roblib_parsing::Writable::write_text(&#name, w)?;
         });
         binary.append_all(quote! {
-            #name.write_binary(w)?;
+            ::roblib_parsing::Writable::write_binary(&#name, w)?;
         });
         binary_async.append_all(quote! {
-            #name.write_binary_async(w).await?;
+            ::roblib_parsing::Writable::write_binary_async(&#name, w).await?;
         });
     }
 
     let res = quote! {
         #[cfg_attr(feature = "async", ::async_trait::async_trait)]
-        impl Writable for #struct_ident {
-            fn write_text(&self, f: &mut dyn ::std::ops::FnMut(&str)) -> ::std::fmt::Result {
+        impl ::roblib_parsing::Writable for #struct_ident {
+            fn write_text(&self, w: &mut dyn std::fmt::Write) -> ::std::fmt::Result {
                 #text
                 Ok(())
             }
@@ -272,7 +272,7 @@ fn get_writable_enum(ident: &Ident, data: &syn::DataEnum) -> TokenStream {
         match data.variants.len() {
             0 => abort!(
                 ident.span(),
-                "Can't implement Writable for enum without variants"
+                "Can't implement ::roblib_parsing::Writable for enum without variants"
             ),
             0x01..=0xFF => "u8",
             0x01_00..=0xFF_FF => "u16",
@@ -311,13 +311,13 @@ fn get_writable_enum(ident: &Ident, data: &syn::DataEnum) -> TokenStream {
             variant_args.append_all(quote! { #name, });
 
             variant_text.append_all(quote! {
-                #name.write_text(f)?;
+                ::roblib_parsing::Writable::write_text(&#name, w)?;
             });
             variant_binary.append_all(quote! {
-                #name.write_binary(w)?;
+                ::roblib_parsing::Writable::write_binary(&#name, w)?;
             });
             variant_binary_async.append_all(quote! {
-                #name.write_binary_async(w).await?;
+                ::roblib_parsing::Writable::write_binary_async(&#name, w).await?;
             });
         }
 
@@ -332,21 +332,21 @@ fn get_writable_enum(ident: &Ident, data: &syn::DataEnum) -> TokenStream {
         text.append_all(quote! {
             #ident :: #variant_name #variant_args => {
                 let tag: #tag_type = #i;
-                tag.write_text(f)?;
+                ::roblib_parsing::Writable::write_text(&tag, w)?;
                 #variant_text
             },
         });
         binary.append_all(quote! {
             #ident :: #variant_name #variant_args => {
                 let tag: #tag_type = #i;
-                tag.write_binary(w)?;
+                ::roblib_parsing::Writable::write_binary(&tag, w)?;
                 #variant_binary
             },
         });
         binary_async.append_all(quote! {
             #ident :: #variant_name #variant_args => {
                 let tag: #tag_type = #i;
-                tag.write_binary_async(w).await?;
+                ::roblib_parsing::Writable::write_binary_async(&tag, w).await?;
                 #variant_binary_async
             },
         });
@@ -354,8 +354,8 @@ fn get_writable_enum(ident: &Ident, data: &syn::DataEnum) -> TokenStream {
 
     let res = quote! {
         #[cfg_attr(feature = "async", ::async_trait::async_trait)]
-        impl Writable for #ident {
-            fn write_text(&self, f: &mut dyn ::std::ops::FnMut(&str)) -> ::std::fmt::Result {
+        impl ::roblib_parsing::Writable for #ident {
+            fn write_text(&self, w: &mut dyn std::fmt::Write) -> ::std::fmt::Result {
                 match self {
                     #text
                 }
@@ -401,6 +401,39 @@ pub fn derive_command(item: proc_macro::TokenStream) -> proc_macro::TokenStream 
         impl From<crate::cmd::Concrete> for #ident {
             fn from(value: crate::cmd::Concrete) -> Self {
                 if let crate::cmd::Concrete::#ident(m) = value {
+                    m
+                } else {
+                    panic!("Tried to convert an unknown command to a concrete command")
+                }
+            }
+        }
+    };
+
+    // println!("{res}");
+    proc_macro::TokenStream::from(res)
+}
+
+#[proc_macro_error]
+#[proc_macro_derive(Event)]
+pub fn derive_event(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let inp = parse_macro_input!(item as DeriveInput);
+    let ident = &inp.ident;
+
+    let readable = get_readable(ident, &inp.data);
+    let writable = get_writable(ident, &inp.data);
+
+    let res = quote! {
+        #readable
+        #writable
+
+        impl From<#ident> for crate::event::Concrete {
+            fn from(value: #ident) -> Self {
+                crate::event::Concrete::#ident(value)
+            }
+        }
+        impl From<crate::event::Concrete> for #ident {
+            fn from(value: crate::event::Concrete) -> Self {
+                if let crate::event::Concrete::#ident(m) = value {
                     m
                 } else {
                     panic!("Tried to convert an unknown command to a concrete command")
