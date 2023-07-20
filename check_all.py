9c9efd7ef2e615778b8821ab89e7733c43ac51e4
beni69 <73666440+beni69@users.noreply.github.com>
2023-07-21 00:58:32 +0200

    ci: check all feature combos

--- check_all.py
+++ check_all.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 
 from itertools import combinations
-from os import system
+from os import system, environ
 import argparse
 
 def flatten(l):
-    return [
+	return [
 		item for sublist in l
 			for item in sublist
 	]
 
 def main():
+	is_ci = bool(environ.get("GITHUB_ACTIONS"))
+
 	p = argparse.ArgumentParser(description='Test all combinations of flags', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 	p.add_argument('-d', '--do', action='store_true', help='actually run cargo check')
 	p.add_argument('-v', '--verbose', action='store_true', help='increase verbosity')
 	p.add_argument('-e', '--examples', action='store_true', help='run for examples')
+	p.add_argument('-f', '--fail', action='store_true', help='Exit on fail')
 	p.add_argument('crate', choices=['roblib', 'roblib-server', 'roblib-client'], help='The crate to test')
 	p.add_argument('features', nargs='+', help='The features to test')
 
@@ -35,13 +38,18 @@ def main():
 		if config.examples:
 			cmd += ' --examples'
 
+		if is_ci:
+			print('##[group]', end='')
 		print(f'{i + 1}/{len(feature_combinations)} {cmd}')
 
 		if config.do:
 			exit_code = system(cmd)
-			if exit_code != 0:
+			if exit_code != 0 and config.fail:
 				exit(1)
 			print('\n\n')
+
+		if is_ci:
+			print('##[endgroup]')
 
 if __name__ == '__main__':
 	main()
